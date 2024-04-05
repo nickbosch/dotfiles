1f6ac52277dc617d524b51c786bb64a8b9422ed2
Nick Bosch <nick.bosch@brainfi.sh>
2024-04-05 16:53:02 +1100

    tweak fish config

--- .config/fish/functions/dotfiles.fish
+++
@@ -1,3 +0,0 @@
-function dotfiles --wraps='/usr/bin/git --git-dir=/Users/nickbosch/.dotfiles --work-tree=/Users/nickbosch' --description 'alias dotfiles=/usr/bin/git --git-dir=/Users/nickbosch/.dotfiles --work-tree=/Users/nickbosch'
-  /usr/bin/git --git-dir=/Users/nickbosch/.dotfiles --work-tree=/Users/nickbosch $argv; 
-end
